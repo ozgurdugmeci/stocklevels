ff27962c06e9186bdaacc6003e44369a122312e2
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2025-12-01 13:03:55 +0300

    Update path.py

--- path.py
+++ path.py
@@ -1,1 +1,4 @@
 import streamlit as st
+
+urly= f'<a target="_blank" rel="noopener noreferrer" href="https://analysethis.streamlit.app">Click to visit ManageStockLevels .</a>'
+st.markdown(urly,unsafe_allow_html=Tr
