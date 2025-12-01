53b99c351516cc224544a4bccce8fe0675788420
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2025-12-01 13:40:02 +0300

    Update empty.py

--- pages/empty.py
+++ pages/empty.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
-'hey'
+'ManageStockLevels just has been moved to https://analysethis.streamlit.app'
+urly= f'<a target="_blank" rel="noopener noreferrer" href="https://analysethis.streamlit.app">Click to visit ManageStockLevels .</a>'
+st.markdown(urly,unsafe_allow_html=True)
