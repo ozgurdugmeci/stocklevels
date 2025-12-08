0f7e89d467aa149c84f0425d0a3c5fdcc7f0c3ee
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2025-12-08 15:47:26 +0300

    Update path.py

--- path.py
+++ path.py
@@ -1,4 +1,4 @@
 import streamlit as st
-'ManageStockLevels just has been moved to https://analysethis.streamlit.app'
+'ManageStockLevels just has been moved to https://analyzethis.streamlit.app'
 urly= f'<a target="_blank" rel="noopener noreferrer" href="https://analysethis.streamlit.app">Click to visit ManageStockLevels .</a>'
 st.markdown(urly,unsafe_allow_html=True)
