22fbb19047acbf8a86bf4b81f5748b9680572724
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2026-04-14 19:48:38 +0300

    Update empty.py
    After webhook

--- pages/empty.py
+++ pages/empty.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-'ManageStockLevels just has been moved to https://analysethis.streamlit.app'
+'-->ManageStockLevels just has been moved to https://analysethis.streamlit.app'
 urly= f'<a target="_blank" rel="noopener noreferrer" href="https://analyzethis.streamlit.app">Click to visit ManageStockLevels .</a>'
 st.markdown(urly,unsafe_allow_html=True)
